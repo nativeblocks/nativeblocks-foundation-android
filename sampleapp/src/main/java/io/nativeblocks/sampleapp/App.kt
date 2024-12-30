85e536dcb262e83b266ad2b87f9c76002d7751ad
alireza <alirezat775@gmail.com>
2024-12-30 10:03:04 +0300

    add ExceptionHandler for sample app to catch UncaughtExceptions and clearFrame cache

---
+++ sampleapp/src/main/java/io/nativeblocks/sampleapp/App.kt
@@ -0,0 +1,54 @@
+package io.nativeblocks.sampleapp
+
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import io.nativeblocks.core.api.NativeblocksEdition
+import io.nativeblocks.core.api.NativeblocksManager
+import io.nativeblocks.foundation.integration.consumer.block.FoundationBlockProvider
+import io.nativeblocks.wandkit.liveKit
+import kotlinx.coroutines.runBlocking
+import kotlin.system.exitProcess
+
+private const val NATIVEBLOCKS_API_KEY = ""
+private const val NATIVEBLOCKS_API_URL = "https://edge.api.nativeblocks.io/gateway"
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(this.applicationContext))
+
+        NativeblocksManager.initialize(
+            applicationContext = this,
+            edition = NativeblocksEdition.Cloud(
+                endpoint = NATIVEBLOCKS_API_URL,
+                apiKey = NATIVEBLOCKS_API_KEY,
+                developmentMode = true
+            )
+            /*
+            edition = NativeblocksEdition.Community(
+                mapOf(
+                    "/login" to "https://nativeblocksjsonschema.nyc3.digitaloceanspaces.com/login.json",
+                    "/profile" to "https://nativeblocksjsonschema.nyc3.digitaloceanspaces.com/profile.json",
+                ),
+            )
+            */
+        )
+        NativeblocksManager.getInstance().liveKit()
+
+        FoundationBlockProvider.provideBlocks()
+        NativeblocksManager.getInstance().provideEventLogger("LOGGER", AppLogger())
+    }
+}
+
+class ExceptionHandler(private val context: Context) : Thread.UncaughtExceptionHandler {
+    override fun uncaughtException(t: Thread, e: Throwable) {
+        Log.d("DEBUG STACKTRACE", e.stackTraceToString())
+        runBlocking {
+            NativeblocksManager.getInstance().clearAllFrames()
+            Toast.makeText(context, e.message.orEmpty(), Toast.LENGTH_LONG).show()
+            exitProcess(1)
+        }
+    }
+}
--- sampleapp/src/main/java/io/nativeblocks/sampleapp/MainActivity.kt
+++ sampleapp/src/main/java/io/nativeblocks/sampleapp/MainActivity.kt
@@ -5,43 +5,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.lifecycleScope
-import io.nativeblocks.core.api.NativeblocksEdition
 import io.nativeblocks.core.api.NativeblocksError
 import io.nativeblocks.core.api.NativeblocksFrame
 import io.nativeblocks.core.api.NativeblocksLoading
 import io.nativeblocks.core.api.NativeblocksManager
 import io.nativeblocks.core.api.provider.logger.INativeLogger
-import io.nativeblocks.foundation.integration.consumer.block.FoundationBlockProvider
-import io.nativeblocks.wandkit.liveKit
 import kotlinx.coroutines.launch
-
-private const val NATIVEBLOCKS_API_KEY = ""
-private const val NATIVEBLOCKS_API_URL = "https://edge.api.nativeblocks.io/gateway"
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        NativeblocksManager.initialize(
-            applicationContext = this,
-            edition = NativeblocksEdition.Cloud(
-                endpoint = NATIVEBLOCKS_API_URL,
-                apiKey = NATIVEBLOCKS_API_KEY,
-                developmentMode = true
-            )
-            /*
-            edition = NativeblocksEdition.Community(
-                mapOf(
-                    "/login" to "https://nativeblocksjsonschema.nyc3.digitaloceanspaces.com/login.json",
-                    "/profile" to "https://nativeblocksjsonschema.nyc3.digitaloceanspaces.com/profile.json",
-                ),
-            )
-            */
-        )
-        NativeblocksManager.getInstance().liveKit()
-
-        FoundationBlockProvider.provideBlocks()
 
         lifecycleScope.launch {
             NativeblocksManager.getInstance().getScaffold({
@@ -51,7 +25,6 @@ class MainActivity : ComponentActivity() {
             })
         }
 
-        NativeblocksManager.getInstance().provideEventLogger("LOGGER", AppLogger())
         setContent {
             NativeblocksFrame(
                 frameRoute = "/",
