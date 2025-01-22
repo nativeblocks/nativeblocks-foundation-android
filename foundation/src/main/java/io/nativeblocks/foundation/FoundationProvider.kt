4a2f761d112a2f9293c24bd46cf8f9cb82f37777
Ali Donyaie <alidonyaie@gmail.com>
2025-01-22 17:38:35 +0330

    merge FoundationProvider function

--- foundation/src/main/java/io/nativeblocks/foundation/FoundationProvider.kt
+++ foundation/src/main/java/io/nativeblocks/foundation/FoundationProvider.kt
@@ -5,7 +5,25 @@ import io.nativeblocks.foundation.actions.NativeChangeVariable
 import io.nativeblocks.foundation.integration.consumer.action.FoundationActionProvider
 import io.nativeblocks.foundation.integration.consumer.block.FoundationBlockProvider
 
+/**
+ * Provides foundational types, actions, and blocks to the Nativeblocks framework.
+ * This object serves as a centralized provider for registering types, actions, and blocks
+ * specific to the foundation module.
+ */
 object FoundationProvider {
+    /**
+     * Initializes and registers foundational types, actions, and blocks with the Nativeblocks framework.
+     *
+     * This function performs the following:
+     * - Registers foundational types by invoking `FoundationTypeProvider.provideTypes()`.
+     * - Registers actions, including:
+     *   - [NativeChangeVariable]: An action for modifying variables with dynamic evaluation.
+     *   - [NativeChangeBlockProperty]: An action for changing block properties.
+     * - Registers foundational blocks by invoking `FoundationBlockProvider.provideBlocks()`.
+     *
+     * Call this method to ensure that the foundation module's functionalities are properly
+     * integrated into the Nativeblocks framework.
+     */
     fun provide() {
         FoundationTypeProvider.provideTypes()
         FoundationActionProvider.provideActions(
