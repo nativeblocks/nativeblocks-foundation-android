444bc1ef08dd8f13f7cbbe6fef9633df8cfa88db
Ali Donyaie <alidonyaie@gmail.com>
2025-02-23 14:53:42 +0330

    use actionHandleVariableValue in NativeChangeVariable

--- foundation/src/main/java/io/nativeblocks/foundation/actions/NativeChangeVariable.kt
+++ foundation/src/main/java/io/nativeblocks/foundation/actions/NativeChangeVariable.kt
@@ -9,9 +9,8 @@ import io.nativeblocks.compiler.type.NativeActionProp
 import io.nativeblocks.compiler.type.NativeActionValuePicker
 import io.nativeblocks.compiler.type.Then
 import io.nativeblocks.core.api.provider.action.ActionProps
+import io.nativeblocks.core.util.actionHandleVariableValue
 import io.nativeblocks.core.util.evaluateMixConditionOperator
-import io.nativeblocks.core.util.getVariableValue
-import io.nativeblocks.core.util.parseWithJsonPath
 
 /**
  * A native action that changes a variable's value based on the provided parameters.
@@ -78,18 +77,10 @@ class NativeChangeVariable {
     @NativeActionFunction
     fun invoke(param: Parameter) {
         val data = param.actionProps.trigger?.data.orEmpty()
+        val variable = param.actionProps.variables?.get(data["variableKey"]?.value.orEmpty())
+        if (variable == null) return
 
-        val variableKeyData = param.actionProps.variables?.get(data["variableKey"]?.value.orEmpty())
-
-        var value = param.variableValue
-        val variable = variableKeyData ?: return
-
-        param.actionProps.variables?.forEach { variableItem ->
-            value = value.getVariableValue(variableItem.key, variableItem.value.value)
-        }
-
-        value =
-            value.parseWithJsonPath(param.actionProps.variables, param.actionProps.listItemIndex)
+        var value = actionHandleVariableValue(param.actionProps, param.variableValue) ?: ""
         value = value.evaluateMixConditionOperator(type = variable.type)
         param.onNext(value)
     }
