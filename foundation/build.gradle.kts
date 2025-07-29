88ccb9e3729139c38d3f00db3fbced5068e392fb
Ali Donyaie <ali@nativeblocks.io>
2025-07-29 12:23:21 +0330

    add native checkbox picker menu and radio group

--- foundation/build.gradle.kts
+++ foundation/build.gradle.kts
@@ -110,6 +110,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material:material:1.7.4")
+    implementation("androidx.compose.material:material-icons-extended:1.7.4")
     implementation("androidx.compose.animation:animation:1.7.4")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.4")
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
