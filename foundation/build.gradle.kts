9363eb6a5d611475ef4ef6987bcf8c879ef0ccb2
alireza <alirezat775@gmail.com>
2025-08-30 20:23:40 +0400

    update nativeblocks dependencies

--- foundation/build.gradle.kts
+++ foundation/build.gradle.kts
@@ -115,9 +115,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
     implementation("io.coil-kt:coil-compose:2.6.0")
     //==========================nativeblocks=======================
-    implementation("io.nativeblocks:nativeblocks-android:1.4.0")
-    ksp("io.nativeblocks:nativeblocks-compiler-android:1.2.2")
-    implementation("io.nativeblocks:nativeblocks-compiler-android:1.2.2")
+    implementation("io.nativeblocks:nativeblocks-android:1.5.0")
+    ksp("io.nativeblocks:nativeblocks-compiler-android:1.3.0")
+    implementation("io.nativeblocks:nativeblocks-compiler-android:1.3.0")
 }
 
 object ModuleInfo {
